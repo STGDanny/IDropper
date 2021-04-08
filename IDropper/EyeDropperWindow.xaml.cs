c6bc6d85fe7e8fde9d245aaa4be95fa7454140b7
STGDanny <ste4lthytaco@gmail.com>
2021-04-07 23:31:14 -0400

    Re-factor and UI improvements

---
+++ IDropper/EyeDropperWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿/*
+*	FILE			=>	EyeDropperWindow.xaml.cs
+*	PROJECT			=>	IDropper
+*	PROGRAMMER		=>	Daniel Pieczewski
+*	LAST UPDATE		=>	2021-04-07
+*	DESCRIPTION		=>
+*		The methods(s) in this file make up the interaction logic for the EyeDropperWindow. They handle any events
+*		related to the EyeDropperWindow.
+*/
+
+//Import Usings:
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace IDropper {
+	public partial class EyeDropperWindow : Window {
+		/*
+		*	METHOD		=>	EyeDropperWindow
+		*	DESCRIPTION	=>
+		*		This method is the constructor for the EyeDropperWindow class
+		*
+		*	PARAMETERS	=>
+		*		void	=>	void
+		*/
+		public EyeDropperWindow() => InitializeComponent();
+
+		/*
+		*	METHOD				=>	Window_KeyDown
+		*	DESCRIPTION			=>
+		*		This method is an event handler for the KeyDown event on the window.
+		*		It determines the action to take depending on the key pressed.
+		*
+		*	PARAMETERS			=>
+		*		object sender	=>	Sender of the event.
+		*		KeyEventArgs e	=>	Any event args sent from the sender.
+		*
+		*	RETURNS				=>
+		*		void			=>	void
+		*/
+		private void Window_KeyDown(object sender, KeyEventArgs e) {
+			//IF the key pressed is Escape => Exit the program:
+			if (e.Key == Key.Escape) {
+				Close();
+			}
+
+			//IF the key pressed is Enter => Activate the dropper:
+			else if (e.Key == Key.Enter) {
+				ActivateDropper();
+			}
+		}
+
+		/*
+		*	METHOD		=>	ActivateDropper
+		*	DESCRIPTION	=>
+		*		This method activated the eye dropper and captures the point to interpolate.
+		*
+		*	PARAMETERS	=>
+		*		void	=>	void
+		*
+		*	RETURNS		=>
+		*		void	=>	void
+		*/
+		private void ActivateDropper() {
+			//Get the pixel under the mouse cursor:
+			Mouse.Capture(this);
+			Point selectedPixel = PointToScreen(Mouse.GetPosition(this));
+			Mouse.Capture(null);
+
+			//Create a new ResultsWindow and open it:
+			new ResultsWindow(Calculate.GetColourAtPixel(new System.Drawing.Point((int)selectedPixel.X, (int)selectedPixel.Y))).Show();
+
+			//Close the IDropper window:
+			Close();
+		}
+
+		/*
+		*	METHOD				=>	Window_KeyDown
+		*	DESCRIPTION			=>
+		*		This method is an event handler for the Deactivated event on the window.
+		*		It changes the window border colour to Green to signify un-readiness to perform an eye dropper.
+		*
+		*	PARAMETERS			=>
+		*		object sender	=>	Sender of the event.
+		*		EventArgs e	=>	Any event args sent from the sender.
+		*
+		*	RETURNS				=>
+		*		void			=>	void
+		*/
+		private void Window_Deactivated(object sender, EventArgs e) => Hitbox.Stroke = System.Windows.Media.Brushes.Lime;
+
+		/*
+		*	METHOD				=>	Window_Activated
+		*	DESCRIPTION			=>
+		*		This method is an event handler for the Activated event on the window.
+		*		It changes the window border colour to Red to signify readiness to perform an eye dropper.
+		*
+		*	PARAMETERS			=>
+		*		object sender	=>	Sender of the event.
+		*		EventArgs e		=>	Any event args sent from the sender.
+		*
+		*	RETURNS				=>
+		*		void			=>	void
+		*/
+		private void Window_Activated(object sender, EventArgs e) => Hitbox.Stroke = System.Windows.Media.Brushes.Red;
+	}
+}
--- IDropper/ResultsWindow.xaml.cs
+++ IDropper/ResultsWindow.xaml.cs
@@ -1,38 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+*	FILE			=>	ResultsWindow.xaml.cs
+*	PROJECT			=>	IDropper
+*	PROGRAMMER		=>	Daniel Pieczewski
+*	LAST UPDATE		=>	2021-04-07
+*	DESCRIPTION		=>
+*		The methods(s) in this file make up the interaction logic for the ResultsWindow. They handle any events
+*		related to the ResultsWindow.
+*/
+
+//Import Usings:
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
+//Convenience Usings:
+using Color = System.Drawing.Color;
 
 namespace IDropper {
 	public partial class ResultsWindow : Window {
-
-		public ResultsWindow(System.Drawing.Color pixelColor) {
+		/*
+		*	METHOD			=>	ResultsWindow
+		*	DESCRIPTION		=>
+		*		This method is the constructor for the ResultsWindow class
+		*
+		*	PARAMETERS		=>
+		*		Color point	=>	Colour of the pixel to display the properties of.
+		*/
+		public ResultsWindow(Color pixelColor) {
 			InitializeComponent();
+
+			//Generate the data required:
 			DisplayData(pixelColor);
 		}
 
-		private void DisplayData(System.Drawing.Color pixelColor) {
-			string hexString = $"{pixelColor.R:X2}{pixelColor.G:X2}{pixelColor.B:X2}";
+		/*
+		*	METHOD					=>	DisplayData
+		*	DESCRIPTION				=>
+		*		This method takes a colour and displays it's properties in various formats.
+		*
+		*	PARAMETERS				=>
+		*		Color pixelColor	=>	Colour to display the properties of.
+		*
+		*	RETURNS					=>
+		*		void				=>	void
+		*/
+		private void DisplayData(Color pixelColor) {
+			//Create a new string containing the hex values for the pixel colour:s
+			var hexString = $"{pixelColor.R:X2}{pixelColor.G:X2}{pixelColor.B:X2}";
 
-			ColourDisplay.Fill = new SolidColorBrush(Color.FromArgb(pixelColor.A, pixelColor.R, pixelColor.G, pixelColor.B));
+			//Create a new fill colour from the drawing color:
+			var fillColour = System.Windows.Media.Color.FromArgb(pixelColor.A, pixelColor.R, pixelColor.G, pixelColor.B);
+
+			//Fill the colour display with the colour:
+			ColourDisplay.Fill = new SolidColorBrush(fillColour);
+
+			//Fill the text boxes with the information:
 			RValue.Text = $"R: {pixelColor.R}";
 			GValue.Text = $"G: {pixelColor.G}";
 			BValue.Text = $"B: {pixelColor.B}";
 			HexValue.Text = $"Hex: #{hexString}";
 		}
 
+		/*
+		*	METHOD					=>	NewColourButton_Click
+		*	DESCRIPTION				=>
+		*		This method is an event handler for the click event on the new colour button.
+		*		It re-opens the eye dropper MainWindow.
+		*
+		*	PARAMETERS				=>
+		*		object sender		=>	Sender of the event.
+		*		RoutedEventArgs e	=>	Any event args sent from the sender.
+		*
+		*	RETURNS					=>
+		*		void				=>	void
+		*/
 		private void NewColourButton_Click(object sender, RoutedEventArgs e) {
-			var mw = new MainWindow();
-			mw.Show();
+			//Create new MainWindow to go again:
+			new EyeDropperWindow().Show();
+
+			//Close the results window:
 			Close();
 		}
 	}
